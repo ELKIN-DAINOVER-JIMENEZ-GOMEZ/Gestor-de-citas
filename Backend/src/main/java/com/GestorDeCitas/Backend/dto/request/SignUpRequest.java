54c0e9f1a308c204d9b3068e432c093bac1e04c6
Dainover <jimenezelkin02@gmail.com>
2025-07-26 20:24:23 -0500

    Creacion de dtos

---
+++ Backend/src/main/java/com/GestorDeCitas/Backend/dto/request/SignUpRequest.java
@@ -0,0 +1,27 @@
+package com.GestorDeCitas.Backend.dto.request;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+
+@Data
+public class SignUpRequest {
+private Long id;
+
+@NotBlank
+    @Size(min = 1, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private String email;
+
+    @NotBlank
+    @Size(min = 1, max = 100)
+    private String password;
+
+    private String role;
+
+}
