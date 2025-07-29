0ce6e444fa6bb94b0df84b10886a8022bcf28c2f
Dainover <jimenezelkin02@gmail.com>
2025-07-28 19:51:26 -0500

    Creacion de la clase detalles de usuario y generador de token jwt

--- Backend/src/main/java/com/GestorDeCitas/Backend/security/AuthTokenFilter.java
+++ Backend/src/main/java/com/GestorDeCitas/Backend/security/AuthTokenFilter.java
@@ -1,5 +1,8 @@
 package com.GestorDeCitas.Backend.security;
 
+
+
+import com.GestorDeCitas.Backend.service.UserDetailsServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,18 +10,25 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class AuthTokenFilter  extends OncePerRequestFilter {
     private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
     @Autowired
     private JwtUtilis jwtUtils;
+
+
+    @Autowired private UserDetailsServiceImpl userDetailsService;
+    // ... (logger) ...
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
